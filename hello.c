e89b0cab5b4f5aa33faf7326e2d613a7d78f3351
dgarcia21 <54485855+dgarcia21@users.noreply.github.com>
2019-08-29 08:09:21 -0500

    added major
    added a line to print my major

---
+++ hello.c
@@ -0,0 +1,24 @@
+/**
+ * Author: Dulce Garcia 
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  printf("Hello World!\n");
+
+  return 0;
+}
+int main(int argc, char **argv) {
+
+
+printf("Dulce Garcia\n");
+ +printf ("Undeclared\n");
+return 0;
+}
